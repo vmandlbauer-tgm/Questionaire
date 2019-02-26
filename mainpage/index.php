0ca5c8982549ad74bed2e85cc77580b5764e5f8a
scakir <secakir20@gmail.com>
2019-02-26 12:42:30 +0100

    Update index.php

--- mainpage/index.php
+++ mainpage/index.php
@@ -1,4 +1,3 @@
-
 <!doctype html>
 <html lang="en">
   <head>
@@ -23,8 +22,8 @@
 
   <section class="jumbotron text-center">
     <div class="container">
-      <h1 class="jumbotron-heading">Album example</h1>
-      <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
+      <h1 class="jumbotron-heading">Questionnaire</h1>
+      <p class="lead text-muted">A Template for the meeting in Wednesday.</p>
       <p>
         <a href="#" class="btn btn-primary my-2">Main call to action</a>
         <a href="#" class="btn btn-secondary my-2">Secondary action</a>
