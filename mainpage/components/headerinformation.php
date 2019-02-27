cf6327b7e4d0e25792e442cfa8be41a388c92316
scakir <secakir20@gmail.com>
2019-02-27 16:21:00 +0100

    Code Splitting

---
+++ mainpage/components/headerinformation.php
@@ -0,0 +1,7 @@
+<?php echo' 
+        <div class="col-sm-8 col-md-7 py-4">
+          <h4 class="text-white">About</h4>
+          <p class="text-muted">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
+        </div>
+		'
+?>
