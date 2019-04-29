955a56f0b3d017e59eae0b632cf33825a2b0482e
Viktor Mandlbauer <vmandlbauer@student.tgm.ac.at>
2019-04-29 14:03:07 +0200

    Login/Register function

--- mainpage/connection.php
+++ mainpage/connection.php
@@ -1,103 +1,17 @@
 <?php
-$host = "localhost";
-$username = "root";
-$password = "";
-$database = "shop";
-$message = "";
-try
-{
-    $connect = new PDO("mysql:host=$host; dbname=$database", $username, $password);
-    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    if(isset($_POST["login"]))
-    {
-        if(empty($_POST["username"]) || empty($_POST["password"]))
-        {
-            $message = '<label>All fields are required</label>';
-        }
-        else
-        {
-            $query = "SELECT * FROM kunde WHERE username = :username AND password = :password";
-            $statement = $connect->prepare($query);
-            $statement->execute(
-                array(
-                    'username'     =>     $_POST["username"],
-                    'password'     =>     $_POST["password"]
-                )
-            );
-            $obj = $statement->fetchObject();
-            $count = $statement->rowCount();
-            if($count > 0)
-            {
-                $_SESSION["username"] = $obj->nname;
-                $_SESSION["kid"] =	$obj->kid;
-                header("location:../../LogedUser.php");
-            }
-            else
-            {
-                $message = '<label>Wrong Data</label>';
-            }
-        }
+require 'vendor/autoload.php'; // include Composer's autoloader
+    try{
+    $m = new MongoDB\Client();
+     //echo "Connection to database Successfull!";echo"<br />";
+
+    $db = $m->questionnaire;
+    //echo "Databse loginreg selected";
+    $collection = $db->user; 
+    //echo "Collection userdata Selected Successfully";
     }
-    if(isset($_POST["register"]))
-        if(empty($_POST["username"]) && empty($_POST["password"]) &&  empty($_POST["vorname"]) && empty($_POST["nachname"]) && empty($_POST["telefonnummer"]) && empty($_POST["schuhgröße"]))
-        {
-            $message = '<label>All fields are required</label>';
-        }
-        else
-        {    $query = "INSERT INTO kunde (vname,
-											 nname,
-											 username,
-											 password,
-											 ktelnr,
-											 skischugr)
-											 VALUES
-											 (:vorname,
-											  :nachname,
-											  :username,
-											  :password,
-											  :telefonnummer,
-											  :schuhgr)
-											 ";
-            $statement = $connect->prepare($query);
-
-            $statement->bindParam(':vorname',$_POST["vorname"],PDO::PARAM_STR);
-            $statement->bindParam(':nachname',$_POST["nachname"],PDO::PARAM_STR);
-            $statement->bindParam(':username',$_POST["username"],PDO::PARAM_STR);
-            $statement->bindParam(':password',$_POST["password"],PDO::PARAM_STR);
-            $statement->bindParam(':telefonnummer',$_POST["telefonnummer"],PDO::PARAM_STR);
-            $statement->bindParam(':schuhgr',$_POST["schuhgröße"],PDO::PARAM_INT);
-
-            $statement->execute();
-            $bablla = $connect->lastInsertId();
-
-            echo $bablla;
-            /*$statement->execute(
-                 array(
-                      ':vorname'      =>     $_POST["vorname"],
-                      ':nachname'     =>     $_POST["nachname"],
-                      ':username'     =>     $_POST["username"],
-                      ':password'     =>     $_POST["password"],
-                      ':telefonnummer'=>     $_POST["telefonnummer"],
-                      ':schuhgröße'   =>     $_POST["schuhgröße"]
-                      //'gewicht'      =>     $_POST["gewicht"],
-                      //'fahrweise'    =>     $_POST["fahrweise"]
-                 )
-            );*/
-            $count = $statement->rowCount();
-            if($count > 0)
-            {
-                $_SESSION["username"] = $_POST["vorname"];
-                $_SESSION["kid"] = $bablla;
-                header("location:../../LogedUser.php");
-            }
-            else
-            {
-                $message = '<label>Wrong Data</label>';
-            }
-        }
-}
-catch(PDOException $error)
-{
-    $message = $error->getMessage();
-}
-?> 
+    catch (Exception $e){
+        die("Error. Couldn't connect to the server. Please Check");
+    }
+       session_start();
+	
+?>
